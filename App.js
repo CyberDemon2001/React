d528dcb94f74daff7ffcb868ef792eb3ba83559f
CyberDemon2001 <vaibhavtyagi254@gmail.com>
2025-02-09 20:03:26 +0530

    One 2

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { id: "h1" }, "I am a H1 Tag"),
@@ -9,7 +13,6 @@ const parent = React.createElement("div", { id: "parent" }, [
   ])
 ]);
 
-console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
