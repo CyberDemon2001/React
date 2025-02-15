8dd0d30ae4716d2f7adb5655da09402fd0ae5556
CyberDemon2001 <vaibhavtyagi254@gmail.com>
2025-02-15 22:10:59 +0530

    second child

--- App.js
+++ App.js
@@ -10,6 +10,10 @@ const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { id: "h1" }, "I am a H1 Tag"),
     React.createElement("h2", { id: "h2" }, "I am a H2 Tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "h1" }, "I am a H1 Tag"),
+    React.createElement("h2", { id: "h2" }, "I am a H2 Tag"),
   ])
 ]);
 
